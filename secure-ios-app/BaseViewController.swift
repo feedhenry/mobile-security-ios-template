c39f216f98f60b085de4efc8a06cbac969275f2a
Wei Li <wei.li@redhat.com>
2017-11-03 17:22:01 +0000

    🎉  create the menu

---
+++ secure-ios-app/BaseViewController.swift
@@ -0,0 +1,118 @@
+//
+//  BaseViewController.swift
+//  secure-ios-app
+//
+//  Created by Wei Li on 03/11/2017.
+//  Copyright © 2017 Wei Li. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController, DrawerMenuDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func drawerMenuItemSelectedAtIndex(_ index: Int) {
+        let topViewController : UIViewController = self.navigationController!.topViewController!
+        print("View Controller is : \(topViewController) \n", terminator: "")
+        switch(index){
+        case 0:
+            print("Home\n", terminator: "")
+            
+            self.openViewControllerBasedOnIdentifier("Home")
+            
+            break
+        case 1:
+            print("Play\n", terminator: "")
+            
+            self.openViewControllerBasedOnIdentifier("PlayVC")
+            
+            break
+        default:
+            print("default\n", terminator: "")
+        }
+    }
+    
+    func openViewControllerBasedOnIdentifier(_ strIdentifier:String){
+        let destViewController : UIViewController = self.storyboard!.instantiateViewController(withIdentifier: strIdentifier)
+        
+        let topViewController : UIViewController = self.navigationController!.topViewController!
+        
+        if (topViewController.restorationIdentifier! == destViewController.restorationIdentifier!){
+            print("Same VC")
+        } else {
+            self.navigationController!.pushViewController(destViewController, animated: true)
+        }
+    }
+    
+    func addSlideMenuButton(){
+        let btnShowMenu = UIButton(type: UIButtonType.system)
+        btnShowMenu.setImage(UIImage(named: "ic_view_headline"), for: UIControlState())
+        btnShowMenu.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        btnShowMenu.addTarget(self, action: #selector(BaseViewController.onSlideMenuButtonPressed(_:)), for: UIControlEvents.touchUpInside)
+        let customBarItem = UIBarButtonItem(customView: btnShowMenu)
+        self.navigationItem.leftBarButtonItem = customBarItem;
+    }
+    
+    @objc func onSlideMenuButtonPressed(_ sender : UIButton){
+        if (sender.tag == 10)
+        {
+            // To Hide Menu If it already there
+            self.drawerMenuItemSelectedAtIndex(-1);
+            
+            sender.tag = 0;
+            
+            let viewMenuBack : UIView = view.subviews.last!
+            
+            UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                var frameMenu : CGRect = viewMenuBack.frame
+                frameMenu.origin.x = -1 * UIScreen.main.bounds.size.width
+                viewMenuBack.frame = frameMenu
+                viewMenuBack.layoutIfNeeded()
+                viewMenuBack.backgroundColor = UIColor.clear
+            }, completion: { (finished) -> Void in
+                viewMenuBack.removeFromSuperview()
+            })
+            
+            return
+        }
+        
+        sender.isEnabled = false
+        sender.tag = 10
+        
+        let menuVC : MenuViewController = self.storyboard!.instantiateViewController(withIdentifier: "MenuViewController") as! MenuViewController
+        menuVC.btnMenu = sender
+        menuVC.delegate = self
+        self.view.addSubview(menuVC.view)
+        self.addChildViewController(menuVC)
+        menuVC.view.layoutIfNeeded()
+        
+        
+        menuVC.view.frame=CGRect(x: 0 - UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height);
+        
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            menuVC.view.frame=CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height);
+            sender.isEnabled = true
+        }, completion:nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ secure-ios-app/drawer/MenuViewController.swift
@@ -0,0 +1,131 @@
+//
+//  MenuUIViewController.swift
+//  secure-ios-app
+//
+//  Created by Wei Li on 03/11/2017.
+//  Copyright © 2017 Wei Li. All rights reserved.
+//
+
+import UIKit
+
+protocol DrawerMenuDelegate {
+    func drawerMenuItemSelectedAtIndex(_ index: Int)
+}
+
+struct MenuItem {
+    var title: String
+    var icon: String
+}
+
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    /** Menu options */
+    @IBOutlet var tblMenuOptions: UITableView!
+    
+    /** Transparent button to hide menus */
+    @IBOutlet var btnCloseDrawerOverlay: UIButton!
+    
+    /** Array containing the menu items */
+    var arrayMenuOptions = [MenuItem]()
+    
+    /** Menu button which was tapped to display the menu */
+    var btnMenu: UIButton!
+    
+    /** Delegate of the MenuVC */
+    var delegate: DrawerMenuDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tblMenuOptions.tableFooterView = UIView()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateArrayMenuOptions()
+    }
+    
+    func updateArrayMenuOptions() {
+        arrayMenuOptions.append(MenuItem(title: "Home", icon: "ic_home"))
+        
+        tblMenuOptions.reloadData()
+    }
+    
+    @IBAction func onCloseMenuClick(_ button:UIButton!){
+        btnMenu.tag = 0
+        
+        if (self.delegate != nil) {
+            var index = Int(button.tag)
+            if(button == self.btnCloseDrawerOverlay){
+                index = -1
+            }
+            delegate?.drawerMenuItemSelectedAtIndex(index)
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            self.view.frame = CGRect(x: -UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width,height: UIScreen.main.bounds.size.height)
+            self.view.layoutIfNeeded()
+            self.view.backgroundColor = UIColor.clear
+        }, completion: { (finished) -> Void in
+            self.view.removeFromSuperview()
+            self.removeFromParentViewController()
+        })
+    }
+    
+    /*
+    // MARK: - UITableViewDataSource protocol
+    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cellMenu")!
+        let imageViewTag = 1
+        let titleViewTag = 2
+        
+        cell.selectionStyle = UITableViewCellSelectionStyle.none
+        cell.layoutMargins = UIEdgeInsets.zero
+        cell.preservesSuperviewLayoutMargins = false
+        cell.backgroundColor = UIColor.clear
+        
+        let lblTitle : UILabel = cell.contentView.viewWithTag(titleViewTag) as! UILabel
+        let imgIcon : UIImageView = cell.contentView.viewWithTag(imageViewTag) as! UIImageView
+        
+        imgIcon.image = UIImage(named: arrayMenuOptions[indexPath.row].icon)
+        lblTitle.text = arrayMenuOptions[indexPath.row].title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayMenuOptions.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+
+    /*
+     // MARK: - UITableViewDelegate protocol
+     */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let btn = UIButton(type: UIButtonType.custom)
+        btn.tag = indexPath.row
+        self.onCloseMenuClick(btn)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
